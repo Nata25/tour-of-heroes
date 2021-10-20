10c25a80a8c73749b22e9770ae4a3833f2e0bded
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-20 17:05:10 +0300

    Add navigation, Dashboard and HeroDetails

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,11 +11,24 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-details.component.scss']
 })
 export class HeroDetailsComponent implements OnInit {
-  @Input() hero?: Hero;
+  hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+
+  ) { }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHeroes().subscribe(heroes => {
+      this.hero = heroes.find(h => h.id === id);
+    })
+  }
 
   ngOnInit(): void {
+    this.getHero()
   }
 
 }
